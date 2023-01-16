3dddd674a09a6c66f0df02c515ceb35ef257c921
tjdwns6872 <tjdwns6872@naver.com>
2023-01-16 21:41:03 +0900

    이메일 관련 DTO 생성

---
+++ soloproject/src/main/java/com/kh/soloproject/entity/EmailDto.java
@@ -0,0 +1,15 @@
+package com.kh.soloproject.entity;
+
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class EmailDto {
+	private String userEmail;
+	private String serial;
+	private Date when;
+}
