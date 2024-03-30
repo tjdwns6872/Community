c37afee26999b345e7028e1439db7ac887b8e2b5
tjdwns6872 <tjdwns6872@naver.com>
2024-03-31 01:22:50 +0900

    swagger 추가

---
+++ community/src/main/java/com/simple/community/restcontroller/TestController.java
@@ -0,0 +1,24 @@
+package com.simple.community.restcontroller;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.simple.community.service.TestService;
+
+@RestController
+public class TestController {
+
+	@Autowired
+	public TestService testService;
+	
+	@GetMapping("/test1")
+	public Map<String, Object> test(){
+		Map<String, Object> map = testService.testMybatis();
+		System.out.println(map);
+		return map;
+	}
+}
